0bdff0babe0da255f308f4044240d65b7182c542
Remco Kranenburg <remco@burgsoft.nl>
2021-05-26 21:53:05 +0200

    Clean up repetitive code

--- day01-tyranny/tyranny.c
+++ day01-tyranny/tyranny.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int calculate_recursive_fuel(int fuel) {
-    int additional_fuel = MAX(fuel / 3 - 2, 0);
+int calculate_fuel(int weight, gboolean include_fuel) {
+    int additional_weight = MAX(weight / 3 - 2, 0);
 
-    if(additional_fuel > 0) {
-        additional_fuel += calculate_recursive_fuel(additional_fuel);
+    if(include_fuel && additional_weight > 0) {
+        additional_weight += calculate_fuel(additional_weight, include_fuel);
     }
 
-    return additional_fuel;
+    return additional_weight;
 }
 
 int main(int argc, char *argv[]) {
@@ -26,19 +26,14 @@ int main(int argc, char *argv[]) {
 
     for(;;) {
         int mass = 0;
-        int num_scanned = scanf("%d", &mass);
+        int tokens_scanned = scanf("%d", &mass);
 
-        if(num_scanned >= 0) {
-            int fuel_required = MAX(mass / 3 - 2, 0);
-
-            if(include_fuel) {
-                fuel_required += calculate_recursive_fuel(fuel_required);
-            }
-
-            total_fuel_required += fuel_required;
-        } else {
+        if(tokens_scanned <= 0) {
             break;
         }
+
+        int fuel_required = calculate_fuel(mass, include_fuel);
+        total_fuel_required += fuel_required;
     }
 
     printf("%d\n", total_fuel_required);
