bbc388de3a05c615dfcf83acda14bbc1ae4bbc1d
Remco Kranenburg <remco@burgsoft.nl>
2021-05-26 21:55:01 +0200

    Use gint, gboolean, gchar

--- day01-tyranny/tyranny.c
+++ day01-tyranny/tyranny.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int calculate_fuel(int weight, gboolean include_fuel) {
-    int additional_weight = MAX(weight / 3 - 2, 0);
+gint calculate_fuel(gint weight, gboolean include_fuel) {
+    gint additional_weight = MAX(weight / 3 - 2, 0);
 
     if(include_fuel && additional_weight > 0) {
         additional_weight += calculate_fuel(additional_weight, include_fuel);
@@ -13,26 +13,26 @@ int calculate_fuel(int weight, gboolean include_fuel) {
     return additional_weight;
 }
 
-int main(int argc, char *argv[]) {
-    _Bool include_fuel = 0;
+int main(gint argc, gchar *argv[]) {
+    gboolean include_fuel = FALSE;
 
-    for(int i = 0; i < argc; i++) {
+    for(gint i = 0; i < argc; i++) {
         if(strcmp(argv[i], "--include-fuel") == 0) {
-            include_fuel = 1;
+            include_fuel = TRUE;
         }
     }
 
-    int total_fuel_required = 0;
+    gint total_fuel_required = 0;
 
     for(;;) {
-        int mass = 0;
-        int tokens_scanned = scanf("%d", &mass);
+        gint mass = 0;
+        gint tokens_scanned = scanf("%d", &mass);
 
         if(tokens_scanned <= 0) {
             break;
         }
 
-        int fuel_required = calculate_fuel(mass, include_fuel);
+        gint fuel_required = calculate_fuel(mass, include_fuel);
         total_fuel_required += fuel_required;
     }
 
